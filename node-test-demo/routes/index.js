9292923f4d89999e6df7ce9db264892336e9ae9b
moxiu-fe <18408244552@163.com>
2017-09-27 18:56:36 +0800

    'node-test'

---
+++ node-test-demo/routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+/**
+ * @swagger
+ * /:
+ *   get:
+ *      description: "描述"
+ *      tags:
+ *      - "index"
+ *      produces:
+ *      - "text/palin"
+ *      responses:
+ *        200:
+ *          description: "返回hello swagger"
+ *          schema:
+ *            type: "string"
+ */
+router.get('/', function(req, res, next) {
+    res.append('Content-Type','text/palain')
+    res.status(200)
+    res.send('hello swagger');
+});
+
+module.exports = router;
---
+++ node-test-demo/routes/users.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var router = express.Router();
+
+  /**
+   * @swagger
+   * definitions:
+   *   login:
+   *     type: "object"
+   *     properties:
+   *       username:
+   *         type: "string"
+   *         description: "电话号码"
+   *       age:
+   *         type: "integer"
+   *
+   */
+
+  /**
+   * @swagger
+   * /users/login:
+   *   post:
+   *     description: "描述"
+   *     tags:
+   *     - "users"
+   *     produces:
+   *     - "application/json"
+   *     parameters:
+   *     - in: "body"
+   *       name: "body"
+   *       required: true
+   *       schema:
+   *         type: "object"
+   *         properties:
+   *            phone:
+   *              type: "string"
+   *              description: "电话号码"
+   *            password:
+   *              type: "string"
+   *              description: "密码"
+   *     responses:
+   *       200:
+   *         description: "登录成功，返回用户名和年龄"
+   *         schema:
+   *           $ref: "#/definitions/login"
+   */
+router.post('/login', function(req, res, next) {
+      res.append('Content-Type','application/json')
+      res.status(200)
+      res.send({name:'Bob',age:18});
+});
+
+module.exports = router;
