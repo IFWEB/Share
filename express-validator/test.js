469feb4742ea2a6f2e1f163b788f5fcd56bc353c
chua1989 <chen_hua89@163.com>
2017-10-30 17:39:45 +0800

    参数校验的msg提取

--- express-validator/test.js
+++ express-validator/test.js
@@ -2,24 +2,24 @@ const express = require('express'),
     app = express();
 
 const {
-    check,
+    validationCheck,
     validationResult
 } = require('express-validator/check');
 
-// console.log(check('username'));
+// console.log(validationCheck('username'));
 //静态资源
 app.use(express.static(__dirname));
 
 //普通参数校验
 app.all('/test1', [
-    check('username')
+    validationCheck('username')
     //这里面的方法处了express-validator提供的外，基本都是validator插件提供的：https://github.com/chriso/validator.js
     .isEmail()
     //这个方法用来描述错误信息，但是不推荐
     .withMessage('must be an email'),
 
     // 通常错误信息可以作为第二个参数
-    check('password', 'passwords must be at least 5 chars long and contain one number')
+    validationCheck('password', 'passwords must be at least 5 chars long and contain one number')
     .isLength({ min: 5 })
 ], (req, res, next) => {
     const errors = validationResult(req);
@@ -43,7 +43,7 @@ app.all('/test1', [
 
 //加入自定义校验
 app.all('/test2', [
-    check('username', 'must be an email')
+    validationCheck('username', 'must be an email')
     .isEmail()
     .custom(value => {
         //自定义校验，个人自己写
@@ -91,14 +91,14 @@ app.all('/test2', [
 
 //包装返回，我们不能直接返回插件提供给我们的对象，需要包装一下,这里面使用errors.array()比errors.mapped要好处理
 app.all('/test3', [
-    check('username', 'username must be an email')
+    validationCheck('username', 'username must be an email')
     .isEmail()
 ], (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(422).json({
           code: 1000,
-          message: errors.array()[0].msg,
+          message: errors.array()[0].msg,//或者errors.mapped().username.msg
           data: {}
         });
     } else {
